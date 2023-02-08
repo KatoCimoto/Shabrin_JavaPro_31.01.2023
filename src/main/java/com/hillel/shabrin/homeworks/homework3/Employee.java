f987626f1994b9305444379dc13cb805c5aeeccc
Shabrin Anton <dogue.13@yahoo.com>
2023-02-08 16:47:54 +0200

    homework 3 done

--- src/main/java/com/hillel/shabrin/homeworks/homework3/Employee.java
+++ src/main/java/com/hillel/shabrin/homeworks/homework3/Employee.java
@@ -21,7 +21,47 @@ public class Employee {
         return  "Name: " + flName + '\n' +
                 "Post: " + post + '\n' +
                 "Age: " + age + '\n' +
-                "Phone nubmer: " + phoneNumber + '\n' +
+                "Phone number: " + phoneNumber + '\n' +
                 "Email: " + email + '\n';
+    }
+
+    public String getFlName() {
+        return flName;
+    }
+
+    public void setFlName(String flName) {
+        this.flName = flName;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
