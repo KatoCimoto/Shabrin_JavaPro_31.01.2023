c896fcb5dd902709b4e4731eb9a20f567b9e5d5e
agrok <andrew.ryabchuk@gmail.com>
2023-02-16 21:31:08 +0200

    Update Cat.java

--- src/main/java/com/hillel/shabrin/homeworks/homework4/animal/Cat.java
+++ src/main/java/com/hillel/shabrin/homeworks/homework4/animal/Cat.java
@@ -4,13 +4,24 @@ public class Cat extends Animal{
 
     private static int catCount;
 
+    private int maxDistanceToRun;
+    
+    
     public Cat() {
         super();
+        maxDistanceToRun = 200;
         catCount++;
     }
 
     public Cat(String name) {
         this.name = name;
+        maxDistanceToRun = 200;
+        catCount++;
+    }
+    
+    public Cat(String name, int maxDistanceToRun) {
+        this.name = name;
+        this.maxDistanceToRun = maxDistanceToRun;
         catCount++;
     }
 
@@ -20,7 +31,7 @@ public class Cat extends Animal{
 
     @Override
     public void run(int howLong) {
-        if (howLong <= 200) {
+        if (howLong <= maxDistanceToRun) {
             super.run(howLong);
         } else
             System.out.println(name + " can't run so far");
