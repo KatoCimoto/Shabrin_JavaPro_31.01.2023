b6f5327387fe83f74fa28c73a92ebeb668b48937
Shabrin Anton <dogue.13@yahoo.com>
2023-02-18 19:48:33 +0200

    polymorphism in Java

--- src/main/java/com/hillel/shabrin/homeworks/homework5/Main.java
+++ src/main/java/com/hillel/shabrin/homeworks/homework5/Main.java
@@ -1,15 +1,12 @@
 package com.hillel.shabrin.homeworks.homework5;
 import com.hillel.shabrin.homeworks.homework5.figure.GetAreas;
-import com.hillel.shabrin.homeworks.homework5.runGame.Start;
+
 
 public class Main {
     public static void main(String[] args) {
 
         GetAreas getAreas = new GetAreas();
         getAreas.getAllAreas();
-
-        Start start = new Start();
-        start.start();
 
     }
 }
