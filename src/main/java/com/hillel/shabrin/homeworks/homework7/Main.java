db2f1d86d0099614d4417b21e8642df5590c6337
Shabrin Anton <dogue.13@yahoo.com>
2023-03-02 18:38:16 +0200

    symbols and strings [updated]

--- src/main/java/com/hillel/shabrin/homeworks/homework7/Main.java
+++ src/main/java/com/hillel/shabrin/homeworks/homework7/Main.java
@@ -8,50 +8,53 @@ public class Main {
     public static void main(String[] args) {
 
         //pt 6
-        String[] words = {"apple", "orange", "lemon", "banana",
-                "apricot", "avocado" , "broccoli", "carrot",
-                "cherry", "garlic", "grape", "melon", "leak",
-                "kiwi", "mango", "mushroom", "nut", "olive",
-                " pea", "peanut", "pear", "pepper", "pineapple",
-                "pumpkin", "potato"};
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! You need to guess one word that I thought of.");
-        System.out.println("This word refers to the theme of vegetables and fruits.");
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado" , "broccoli",
+                "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
+                "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
 
         Random random = new Random();
 
-        String answer = words[random.nextInt(words.length)];
-        char[] answerChars = answer.toCharArray();
-        char[] guessChars = new char[answerChars.length];
-        Arrays.fill(guessChars, '#');
+        String word = words[random.nextInt(words.length)];
 
-        boolean guessed = false;
-        while (!guessed) {
-            System.out.print("Guess the word: ");
-            String guess = scanner.nextLine().toLowerCase();
+        int attempts = 0;
 
-            if (guess.equals(answer)) {
-                guessed = true;
+        StringBuilder guess = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            guess.append("#");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Guess the word: " + guess);
+
+            System.out.print("Enter your guess (lowercase only): ");
+            String answer = scanner.nextLine();
+
+            if (answer.equals(word)) {
+                System.out.println("Congratulations! You guessed the word in " + attempts + " attempts.");
+                break;
             } else {
-                for (int i = 0; i < guessChars.length; i++) {
-                    if (i < guess.length() && guess.charAt(i) == answerChars[i]) {
-                        guessChars[i] = answerChars[i];
+                attempts++;
+                System.out.println("Wrong answer. Try again.");
+
+                for (int i = 0; i < word.length(); i++) {
+                    if (i < answer.length() && answer.charAt(i) == word.charAt(i)) {
+                        guess.setCharAt(i, answer.charAt(i));
                     }
                 }
-                System.out.println(guessChars);
             }
         }
-        System.out.println("Congratulations! You guessed the word: " + answer);
+        scanner.close();
     }
+
 
     //pt 5
     public static boolean isPalindrome(String str){
-        StringBuilder strBuilder = new StringBuilder(str);
-        strBuilder.reverse();
-        String strInverted = strBuilder.toString();
 
-        return str.equalsIgnoreCase(strInverted);
+        StringBuilder builder = new StringBuilder(str);
+       return builder.reverse().toString().equalsIgnoreCase(str);
     }
 
     //pt 4
@@ -61,12 +64,8 @@ public class Main {
 
     //pt 3
     public static int findWordPosition(String source, String target){
-        int position = source.indexOf(target);
-        if (position == -1) {
-            return -1;
-        } else {
-            return position;
-        }
+
+        return source.indexOf(target);
     }
 
 
@@ -74,13 +73,11 @@ public class Main {
     public static void findSymbolOccurance(String str, char ch){
 
         int count = 0;
-        char[] strArray = str.toCharArray();
-        for (char c : strArray) {
+        for (char c : str.toCharArray()) {
             if (ch == c) {
                 count++;
             }
         }
-        System.out.println("The number of char \"" + ch + "\" in a string is " + count);
-
+        System.out.println("The number of char '" + ch + "' in a string is " + count);
     }
 }
